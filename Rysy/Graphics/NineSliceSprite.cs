1ecffbd2dcd51ccbc9c30ce47634b38e01513f5f
JaThePlayer <janmalenta@gmail.com>
2025-02-24 23:12:38 +0100

    [Lua] ninePatchOptions support

--- Rysy/Graphics/NineSliceSprite.cs
+++ Rysy/Graphics/NineSliceSprite.cs
@@ -1,5 +1,4 @@
-﻿using Rysy.Extensions;
-using Rysy.Selections;
+﻿using Rysy.Selections;
 
 namespace Rysy.Graphics;
 
@@ -16,9 +15,25 @@ public record struct NineSliceSprite : ISprite {
 
     public bool IsLoaded => Texture.Texture is { };
 
+    public LoopingModes BorderMode { get; set; } = LoopingModes.Repeat;
+    public LoopingModes FillMode { get; set; } = LoopingModes.Repeat;
+
+    public RenderModes RenderMode { get; set; } = RenderModes.Fill;
+
+    public int TileWidth { get; set; } = 8;
+    
+    public int TileHeight { get; set; } = 8;
+    
     public ISprite WithMultipliedAlpha(float alpha) {
         return this with {
             Color = Color * alpha,
+        };
+    }
+
+    private static int GetMiddleSubtextureX(LoopingModes mode, Vector2 pos, int xIdx, int textureWidth, int tileWidth) {
+        return mode switch {
+            LoopingModes.Random => pos.SeededRandomInclusive(1, (textureWidth / tileWidth) - 2) * tileWidth,
+            _ => ((xIdx - 1) % (textureWidth / tileWidth - 2) + 1) * tileWidth,
         };
     }
 
@@ -27,43 +42,69 @@ public record struct NineSliceSprite : ISprite {
             return;
 
         var b = GFX.Batch;
-        const int tileSize = 8;
 
         var rect = Pos;
         var w = rect.Width;
-        var wBy8 = w / tileSize;
+        var wBy8 = w / TileWidth;
         var h = rect.Height;
-        var hBy8 = h / tileSize;
+        var hBy8 = h / TileHeight;
         var pos = rect.Location.ToVector2();
         var c = Color;
-
         
         var texWidth = Texture.Width;
         var texHeight = Texture.Height;
-
-        for (int x = 0; x < wBy8; x++) {
-            var left = x == 0;
-            var middle = x + 1 < wBy8;
-
-            Rectangle subtext;
-            Vector2 offset;
-
-            var subX = left ? 0 : middle ? tileSize : texWidth - tileSize;
-
+        
+        Rectangle subtext;
+        Vector2 offset;
+        
+        // Corners
+        subtext = Texture.GetSubtextureRect(0, 0, TileWidth, TileHeight, out offset);
+        b.Draw(texture, pos + offset, subtext, c);
+        subtext = Texture.GetSubtextureRect(texWidth - TileWidth, 0, TileWidth, TileHeight, out offset);
+        b.Draw(texture, pos.AddX(w - TileWidth) + offset, subtext, c);
+        subtext = Texture.GetSubtextureRect(texWidth - TileWidth, texHeight - TileHeight, TileWidth, TileHeight, out offset);
+        b.Draw(texture, pos.AddX(w - TileWidth).AddY(h - TileHeight) + offset, subtext, c);
+        subtext = Texture.GetSubtextureRect(0, texHeight - TileHeight, TileWidth, TileHeight, out offset);
+        b.Draw(texture, pos.AddY(h - TileHeight) + offset, subtext, c);
+        
+        // Top and Bottom
+        for (int x = 1; x < wBy8 - 1; x++) {
             // top
-            subtext = Texture.GetSubtextureRect(subX, 0, tileSize, tileSize, out offset);
-            b.Draw(texture, pos.AddX(x * tileSize) + offset, subtext, c);
-
+            var renderPos = pos.AddX(x * TileWidth);
+            subtext = Texture.GetSubtextureRect(GetMiddleSubtextureX(BorderMode, renderPos, x, texWidth, TileWidth), 0, TileWidth, TileHeight, out offset);
+            b.Draw(texture, renderPos + offset, subtext, c);
+            
+            // bottom
             if (h > 8) {
-                // bottom
-                subtext = Texture.GetSubtextureRect(subX, texHeight - tileSize, tileSize, tileSize, out offset);
-                b.Draw(texture, pos.Add(x * tileSize, h - tileSize) + offset, subtext, c);
+                renderPos = pos.Add(x * TileWidth, h - TileHeight);
+                subtext = Texture.GetSubtextureRect(GetMiddleSubtextureX(BorderMode, renderPos, x, texWidth, TileWidth), texHeight - TileHeight, TileWidth, TileHeight, out offset);
+                b.Draw(texture, renderPos + offset, subtext, c);
             }
+        }
+        
+        // Left and Right
+        for (int y = 1; y < hBy8 - 1; y++) {
+            // left
+            var renderPos = pos.AddY(y * TileHeight);
+            subtext = Texture.GetSubtextureRect(0, GetMiddleSubtextureX(BorderMode, renderPos, y, texHeight, TileHeight), TileWidth, TileHeight, out offset);
+            b.Draw(texture, renderPos + offset, subtext, c);
+            
+            // right
+            if (w > 8) {
+                renderPos = pos.Add(w - TileWidth, y * TileWidth);
+                subtext = Texture.GetSubtextureRect(texWidth - TileWidth, GetMiddleSubtextureX(BorderMode, renderPos, y, texHeight, TileHeight), TileWidth, TileHeight, out offset);
+                b.Draw(texture, renderPos + offset, subtext, c);
+            }
+        }
 
-            for (int y = 1; y < hBy8 - 1; y++) {
-                // middle
-                subtext = Texture.GetSubtextureRect(subX, tileSize, tileSize, tileSize, out offset);
-                b.Draw(texture, pos.Add(x * tileSize, y * tileSize) + offset, subtext, c);
+        if (RenderMode != RenderModes.Border) {
+            // Middle
+            for (int x = 1; x < wBy8 - 1; x++) {
+                for (int y = 1; y < hBy8 - 1; y++) {
+                    var renderPos = pos.Add(x * TileWidth, y * TileWidth);
+                    subtext = Texture.GetSubtextureRect(GetMiddleSubtextureX(BorderMode, renderPos, x, texWidth, TileWidth), GetMiddleSubtextureX(BorderMode, renderPos,  y,texHeight, TileHeight), TileWidth, TileHeight, out offset);
+                    b.Draw(texture, renderPos + offset, subtext, c);
+                }
             }
         }
     }
@@ -79,9 +120,20 @@ public record struct NineSliceSprite : ISprite {
 
     public ISelectionCollider GetCollider()
         => ISelectionCollider.FromRect(Pos);
-}
+    
+    public enum LoopingModes {
+        Repeat,
+        Random,
+    }
 
-public enum LoopingMode {
-    Repeat,
-
+    public enum RenderModes {
+        /// <summary>
+        /// Renders both the border and the inside of the entity, default.
+        /// </summary>
+        Fill,
+        /// <summary>
+        /// Only renders the border around the sprite, leaving the center empty
+        /// </summary>
+        Border,
+    }
 }
