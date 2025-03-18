2f349b55984366b0b074f944f77b3ad8e249b148
JaThePlayer <janmalenta@gmail.com>
2025-03-18 17:36:14 +0100

    [Lua] Dropdown `valueTransformer` and `displayTransformer`

---
+++ Rysy/LuaSupport/LonnFieldType.cs
@@ -0,0 +1,21 @@
+﻿using KeraLua;
+
+namespace Rysy.LuaSupport;
+
+public sealed class LonnFieldType {
+    public string Name { get; private set; }
+    
+    public LuaFunctionRef GetElement { get; private set; }
+
+    public LonnFieldType(LuaCtx ctx) {
+        var lua = ctx.Lua;
+        var top = lua.GetTop();
+        
+        if (lua.Type(top) != LuaType.Table) {
+            throw new Exception($"File did not return a table");
+        }
+        
+        Name = lua.PeekTableStringValue(top, "fieldType"u8)  ?? throw new Exception("Field type does not have a `fieldType` string value");
+        GetElement = lua.PeekTableFunctionValue(top, "getElement"u8) ?? throw new Exception("Field type does not have a `getElement` function");
+    }
+}
